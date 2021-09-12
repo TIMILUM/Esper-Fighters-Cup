c89ae839826ea17ac7e0f5dc31e5e2667ceb44f4
ounols <ounols@naver.com>
2021-09-13 02:02:35 +0900

    주석 내 오타 수정

--- Esper-Fighters-Cup/Assets/Scripts/Controller/MovementController.cs
+++ Esper-Fighters-Cup/Assets/Scripts/Controller/MovementController.cs
@@ -10,7 +10,7 @@ public class MovementController : ControllerBase
     private BuffController _buffController = null;
     private void Reset()
     {
-        // 컨트롤러 타입 지정을 위해 Awake 함수로 이렇게 선언을 해줘야 합니다.
+        // 컨트롤러 타입 지정을 위해 Reset 함수로 이렇게 선언을 해줘야 합니다.
         // 리플렉션으로 전환할 예정 (IL2CPP 모듈 추가가 필요하기 때문에 나중에 전환할 예정)
         SetControllerType(ControllerManager.Type.MovementController);
     }
