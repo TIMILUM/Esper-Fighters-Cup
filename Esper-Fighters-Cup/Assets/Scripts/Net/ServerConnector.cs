455ba0d50b3cc78876f1de2e8320fddab4d2d9e8
Pilgyeong Gwak <pid011@kakao.com>
2021-09-19 05:07:08 +0900

    서버 연결 순서 변경 및 이름 설정 기능 추가

---
+++ Esper-Fighters-Cup/Assets/Scripts/Net/ServerConnector.cs
@@ -0,0 +1,33 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ServerConnector : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private Text _statusText;
+
+    private void Awake()
+    {
+        Debug.Assert(_statusText);
+        _statusText.text = string.Empty;
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        _statusText.text = "서버와 연결되었습니다!";
+        CoroutineTimer.SetTimerOnce(() => SceneManager.LoadScene("MatchScene"), 0.5f);
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        base.OnDisconnected(cause);
+    }
+
+    public void Connect()
+    {
+        _statusText.text = "서버와 연결을 시도합니다...";
+        PhotonNetwork.ConnectUsingSettings();
+    }
+}
---
+++ Esper-Fighters-Cup/Assets/Scripts/Util/CoroutineTimer.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// 코루틴으로 동작하는 타이머입니다.
+/// </summary>
+public class CoroutineTimer : MonoBehaviour
+{
+    private static CoroutineTimer Instance
+    {
+        get
+        {
+            if (s_instance == null)
+            {
+                var timerObject = Resources.Load<GameObject>("Prefabs/UtilPrefabs/CoroutineTimer");
+                s_instance = Instantiate(timerObject).GetComponent<CoroutineTimer>();
+            }
+
+            return s_instance;
+        }
+    }
+    private static CoroutineTimer s_instance;
+
+    /// <summary>
+    /// 단 한번만 동작하는 타이머
+    /// </summary>
+    /// <param name="timerAction">실행할 메소드</param>
+    /// <param name="delay">메소드 실행 전 딜레이 시간</param>
+    public static void SetTimerOnce(UnityAction timerAction, float delay)
+    {
+        Instance.StartCoroutine(SetTimerOnceCoroutine(timerAction, delay));
+    }
+
+    /// <summary>
+    /// 반복적으로 동작하는 타이머
+    /// </summary>
+    /// <param name="timerAction">실행할 메소드</param>
+    /// <param name="interval">메소드 실행 시간 간격</param>
+    /// <param name="startDelay">최초 실행 전 딜레이 시간</param>
+    /// <returns></returns>
+    public static Coroutine SetTimerRepeat(UnityAction timerAction, float interval, float startDelay)
+    {
+        return Instance.StartCoroutine(SetTimerRepeatCoroutine(timerAction, interval, startDelay));
+    }
+
+    private static IEnumerator SetTimerOnceCoroutine(UnityAction timerAction, float delay)
+    {
+        if (timerAction is null)
+        {
+            yield break;
+        }
+
+        yield return new WaitForSeconds(delay);
+        timerAction();
+    }
+
+    private static IEnumerator SetTimerRepeatCoroutine(UnityAction timerAction, float interval, float startDelay)
+    {
+        if (timerAction is null)
+        {
+            yield break;
+        }
+
+        var wait = new WaitForSeconds(interval);
+
+        yield return new WaitForSeconds(startDelay);
+        while (true)
+        {
+            timerAction();
+            yield return wait;
+        }
+    }
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+}
