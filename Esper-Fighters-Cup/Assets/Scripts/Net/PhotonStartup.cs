171fd1bc0daf17b281d34e4fd864491f7548c865
Pilgyeong Gwak <pid011@kakao.com>
2021-09-21 18:37:11 +0900

    매칭 기능 구현

---
+++ Esper-Fighters-Cup/Assets/Scripts/Net/PhotonStartup.cs
@@ -0,0 +1,16 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+public class PhotonStartup : MonoBehaviourPunCallbacks
+{
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+    private static void InitPhoton()
+    {
+        // 포톤의 게임버전은 프로젝트의 버전을 따라갑니다.
+        PhotonNetwork.GameVersion = Application.version;
+        Debug.Log($"Game Version: {PhotonNetwork.GameVersion}");
+
+        // 방에 입장한 플레이어와 씬 동기화
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+}
