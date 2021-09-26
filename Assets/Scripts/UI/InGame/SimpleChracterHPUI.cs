5ecaf8ff0d5fa8fe01ee6a921696e2d83a35f440
Choi Hyeong Sun <chlgudtjn7@naver.com>
2021-09-27 08:41:37 +0900

    캐릭터 Hp 감소 및 간단한 Hp Ui

---
+++ Assets/Scripts/UI/InGame/SimpleChracterHPUI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class ChracterHpUIStruct
+{
+    public Text Text;
+    public ACharacter Chracter;
+    public string Name;
+}
+
+public class SimpleChracterHPUI : MonoBehaviour
+{
+
+    [SerializeField] private List<ChracterHpUIStruct> ChracterUIStruct;
+
+    private void Update()
+    {
+        foreach (var data in ChracterUIStruct)
+        {
+            data.Text.text = data.Name + " : " + data.Chracter.CharacterHP.ToString();
+        }
+    }
+}
