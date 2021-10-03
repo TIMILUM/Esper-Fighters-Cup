88e97ca8594515c53ef1156d908a05d8715b1aea
ounols <ounols@naver.com>
2021-10-03 20:59:52 +0900

    톱날 기믹 시스템 간단하게 구현

---
+++ Assets/Scripts/Manager/SawBladeSystem.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class SawBladeSystem : MonoBehaviour
+{
+    [SerializeField]
+    private SawBladeObject _sawBladeObjectPrefab = null;
+
+    [SerializeField]
+    private Transform _positionRoot = null;
+
+    [SerializeField]
+    private Transform _sawBladeRoot = null;
+    
+    private List<SawBladePosition> _bladePositions = new List<SawBladePosition>();
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        // 알아서 하위 오브젝트의 포지션 데이터를 들고옴
+        for (var i = 0; i < _positionRoot.childCount; ++i)
+        {
+            var child = _positionRoot.GetChild(i);
+            var start = child.Find("start");
+            var end = child.Find("end");
+            
+            _bladePositions.Add(new SawBladePosition
+            {
+                _startTransform = start, 
+                _endTransform = end
+            });
+        }
+    }
+    
+    /// <summary>
+    /// 톱날 오브젝트를 생성합니다.
+    /// </summary>
+    public void GenerateSawBladeObject()
+    {
+        var sawBladeObject = Instantiate(_sawBladeObjectPrefab, _sawBladeRoot);
+        var (start, end) = GetSawBladePositions();
+        sawBladeObject.SetDirection(start, end);
+    }
+
+    private (Transform, Transform) GetSawBladePositions()
+    {
+        var index = Random.Range(0, _bladePositions.Count);
+        return (_bladePositions[index]._startTransform, _bladePositions[index]._endTransform);
+    }
+
+    private class SawBladePosition
+    {
+        public Transform _startTransform;
+        public Transform _endTransform;
+    }
+}
