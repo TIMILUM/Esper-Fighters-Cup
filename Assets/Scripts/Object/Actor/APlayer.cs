aa0f12ad888da665bf7d95d51a19dd1e9e1acfbc
ounols <ounols@naver.com>
2021-10-07 19:49:24 +0900

    [issues#23] 플레이어 오브젝트 삭제 시 CameraMovement에서 뜨는 예외 처리 적용

--- Assets/Scripts/Object/Actor/APlayer.cs
+++ Assets/Scripts/Object/Actor/APlayer.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class APlayer : ACharacter
@@ -5,14 +6,20 @@ public class APlayer : ACharacter
     [SerializeField, Tooltip("직접 컴포넌트를 넣어주세요.")]
     private Rigidbody _rigidbody = null;
 
+    private CameraMovement _cameraMovement;
+
     // Start is called before the first frame update
     protected override void Start()
     {
         base.Start();
-        _rigidbody.isKinematic = !(photonView?.IsMine) ?? false;
+        _rigidbody.isKinematic = !photonView?.IsMine ?? false;
+        _cameraMovement = Camera.main.gameObject.GetComponent<CameraMovement>();
+        _cameraMovement.AddTarget(transform); // 카메라 타겟 추가 설정
+    }
 
-        Camera.main.gameObject.GetComponent<CameraMovement>().AddTarget(transform); // 카메라 타겟 추가 설정
-
+    private void OnDestroy()
+    {
+        _cameraMovement.RemoveTarget(transform); // 카메라 타겟 삭제
     }
 
     // Update is called once per frame
