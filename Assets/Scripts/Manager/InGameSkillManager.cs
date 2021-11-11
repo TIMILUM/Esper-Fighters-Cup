96a7b3f7278fef29511955760fccd5db98cd8903
pid011 <pid011@kakao.com>
2021-11-12 00:16:59 +0900

    InGameSkillManager를 Singleton<>을 사용하도록 수정

--- Assets/Scripts/Manager/InGameSkillManager.cs
+++ Assets/Scripts/Manager/InGameSkillManager.cs
@@ -1,40 +1,18 @@
+using EsperFightersCup.Util;
 using UnityEngine;
-
 
 /// <summary>
 /// 스킬에서 만들어내는 오브젝트를 관리하기 위해서
 /// 싱글톤으로 만들었습니다.
 /// </summary>
-public class InGameSkillManager : MonoBehaviour
+public class InGameSkillManager : Singleton<InGameSkillManager>
 {
-    private static InGameSkillManager s_instance;
-
     [SerializeField]
     private FragmentAreaManager _fragmentArea;
 
     [SerializeField]
     private SkillObjectFactory _skillObjectfactory;
 
-
-    public static InGameSkillManager Instance
-    {
-        get
-        {
-            if (s_instance == null)
-                return null;
-
-            return s_instance;
-        }
-    }
-
-    // Start is called before the first frame update
-    private void Awake()
-    {
-        if (s_instance == null)
-            s_instance = this;
-
-
-    }
     /// <summary>
     /// 파편지대 생성
     /// </summary>
@@ -44,6 +22,7 @@ public class InGameSkillManager : MonoBehaviour
     {
         _fragmentArea.AddFragmentList(trans, range, ActorID);
     }
+
     /// <summary>
     /// 파편지대 시작 event 띄움
     /// </summary>
@@ -51,6 +30,7 @@ public class InGameSkillManager : MonoBehaviour
     {
         _fragmentArea.EventStart();
     }
+
     /// <summary>
     /// 지금 설치한 파편지대 수
     /// </summary>
@@ -59,6 +39,7 @@ public class InGameSkillManager : MonoBehaviour
     {
         return _fragmentArea.FragmentCount();
     }
+
     /// <summary>
     /// 파편지대 모두 활성화
     /// </summary>
@@ -66,6 +47,7 @@ public class InGameSkillManager : MonoBehaviour
     {
         _fragmentArea.SetFragmentAreaActive(pos, range, ActorID);
     }
+
     /// <summary>
     /// 파편지대 위치 설정한 파편지대 삭제
     /// </summary>
@@ -73,6 +55,7 @@ public class InGameSkillManager : MonoBehaviour
     {
         _fragmentArea.AllDestory();
     }
+
     /// <summary>
     /// 연속 설치 방지하기 위해서 체크하는 함수
     /// </summary>
@@ -87,6 +70,7 @@ public class InGameSkillManager : MonoBehaviour
     {
         _fragmentArea.CurrentFragmentAreaClear();
     }
+
     /// <summary>
     /// 취소함수
     /// </summary>
@@ -95,14 +79,10 @@ public class InGameSkillManager : MonoBehaviour
         _fragmentArea.CancelFragment();
     }
 
-
     public void FragmentClear()
     {
         _fragmentArea.CurrentFragmentAreaClear();
     }
-
-
-
 
     /// <summary>
     /// 오브젝트 생성하는 함수
@@ -114,10 +94,9 @@ public class InGameSkillManager : MonoBehaviour
     {
         return _skillObjectfactory.CreateSkillObject(objectname, pos);
     }
+
     public GameObject CreateSkillUI(string objectname, Vector3 pos)
     {
         return _skillObjectfactory.CreateSkillUI(objectname, pos);
     }
-
-
 }
