d06bc51990b3056a2976c1bf0883dde27883092c
sunghoon331 <sunghoon331@gmail.com>
2021-11-26 03:03:27 +0900

    빌보드 이미지 매니저에 주석 추가

--- Assets/Scripts/DesignerScript/Billborad_ImageChange_Manager.cs
+++ Assets/Scripts/DesignerScript/Billborad_ImageChange_Manager.cs
@@ -7,6 +7,8 @@ namespace EsperFightersCup
     public class Billborad_ImageChange_Manager : MonoBehaviour
     {
 
+        
+        //인스펙터를 통해서 맵에 배치된 5개의 스크린 오브젝트(소형4개, 대형 1개)에 접근
         [SerializeField]
         private GameObject Screen01;
 
@@ -22,6 +24,7 @@ namespace EsperFightersCup
         [SerializeField]
         private GameObject Screen05;
 
+        //기믹이 등장할 때 아래 함수를 호출해주세요.
         public void Billborad_Gimmick_Alarm()
         {
             this.Screen01.GetComponent<Billborad_Image_Changer>().BillboradWorning();
@@ -30,6 +33,6 @@ namespace EsperFightersCup
             this.Screen04.GetComponent<Billborad_Image_Changer>().BillboradWorning();
             this.Screen05.GetComponent<Billborad_Image_Changer>().BillboradWorning();
         }
-
+        //각 오브젝트마다 배치된 "Billborad_Image_Changer" 스크립트 컴포넌트의 퍼블릭 함수 호출
     }
 }
