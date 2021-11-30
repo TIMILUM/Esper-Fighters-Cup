8f6733d7a3baee68ee6f4b7a9c507b0f190293e1
Choi Hyeong Sun <chlgudtjn7@naver.com>
2021-11-30 21:16:38 +0900

    plank 아바타 마스크 변경

---
+++ Assets/Scripts/Controller/ControllerObjects/SkillObject/Plank/GroundSmash.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace EsperFightersCup
+{
+    public class GroundSmash : SkillObject
+    {
+        protected override void BeforeEndDelay()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void BeforeFrontDelay()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void OnCancel()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override UniTask<bool> OnReadyToUseAsync(CancellationToken cancellation)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void OnRelease()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override UniTask OnUseAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
