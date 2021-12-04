941c80dbc40b8b6ac7ce712004e7dccf62bad014
sunghoon331 <sunghoon331@gmail.com>
2021-12-05 08:36:44 +0900

    RandomMatch 스크립트 수정(사운드 오브젝트 액티브 및 디스트로이 처리)

--- Assets/Scripts/UI/Match/RandomMatch.cs
+++ Assets/Scripts/UI/Match/RandomMatch.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+
 namespace EsperFightersCup.UI.Match
 {
     public class RandomMatch : PunEventCallbacks
     {
         [SerializeField] private Text _matchingText;
         [SerializeField] private BasicPopup _popup;
+
+        //FMOD 사운드 이미터를 가진 오브젝트 3종을 인스펙터에서 입력받습니다. (생성 시 사운드 재생. 제거 시 종료 처리)
+        [SerializeField] private GameObject _matchingSound;
+        [SerializeField] private GameObject _failSound;
+        [SerializeField] private GameObject _successSound;
 
         private Coroutine _matchFaildTimer;
 
@@ -20,6 +26,8 @@ namespace EsperFightersCup.UI.Match
             PhotonNetwork.JoinLobby(PhotonOptions.RandomMatchLobby);
 
             CoroutineTimer.SetTimerOnce(StartRandomMatch, 1f);
+
+            _matchingSound.SetActive(true); //매칭 시작시 오브젝트 액티브시켜 사운드 재생
         }
 
         private void StartRandomMatch()
@@ -27,6 +35,8 @@ namespace EsperFightersCup.UI.Match
             if (!PhotonNetwork.IsConnected)
             {
                 PrintRandomMatchFaildMessage("서버와 연결되어 있지 않습니다", "TitleScene");
+                Destroy(_matchingSound);    //매칭 종료에 따라 오브젝트 제거하여 매칭 사운드 제거
+                _failSound.SetActive(true); //실패 사운드 오브젝트 액티브하여 사운드 재생
                 return;
             }
 
@@ -39,6 +49,8 @@ namespace EsperFightersCup.UI.Match
             if (!result)
             {
                 PrintRandomMatchFaildMessage("랜덤 매칭에 실패했습니다", "LobbyScene");
+                Destroy(_matchingSound);    //매칭 종료에 따라 오브젝트 제거하여 매칭 사운드 제거
+                _failSound.SetActive(true); //실패 사운드 오브젝트 액티브하여 사운드 재생
                 return;
             }
         }
@@ -47,11 +59,15 @@ namespace EsperFightersCup.UI.Match
         {
             _matchingText.text = "유저를 찾았습니다!";
             CoroutineTimer.Stop(ref _matchFaildTimer);
+            Destroy(_matchingSound);    //매칭 종료에 따라 오브젝트 제거하여 매칭 사운드 제거
+            _successSound.SetActive(true);  //성공 사운드 오브젝트 액티브하여 사운드 재생
         }
 
         private void OnMatchFailed()
         {
             PrintRandomMatchFaildMessage("유저를 찾지 못했습니다", "LobbyScene");
+            Destroy(_matchingSound);    //매칭 종료에 따라 오브젝트 제거하여 매칭 사운드 제거
+            _failSound.SetActive(true); //실패 사운드 오브젝트 액티브하여 사운드 재생
         }
 
         private void PrintRandomMatchFaildMessage(string cause, string nextScene)
