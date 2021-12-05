716e749fdfb0383193ba64de832c97608a4efc00
pid011 <pid011@kakao.com>
2021-12-05 15:06:49 +0900

    타겟이 제거되면 스킬 UI도 제거되도록 수정

--- Assets/Scripts/UI/InGame/Skill/PlankSlidingUI.cs
+++ Assets/Scripts/UI/InGame/Skill/PlankSlidingUI.cs
@@ -1,30 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 namespace EsperFightersCup.UI.InGame.Skill
 {
     public class PlankSlidingUI : SkillUI
     {
-
-
-
         private void Update()
         {
-            var TargetPos = Target.transform.position;
-            var TargetForword = Target.transform.forward * 2.0f;
+            if (Target == null)
+            {
+                return;
+            }
 
+            var targetPos = Target.transform.position;
+            var targetForword = Target.transform.forward * 2.0f;
 
-            transform.SetPositionAndRotation
-            (TargetPos + TargetForword, Target.transform.rotation);
-
+            transform.SetPositionAndRotation(targetPos + targetForword, Target.transform.rotation);
 
             if (!Target.BuffController.ActiveBuffs.Exists(BuffObject.Type.Sliding))
             {
                 gameObject.SetActive(false);
             }
-
-
         }
     }
 }
