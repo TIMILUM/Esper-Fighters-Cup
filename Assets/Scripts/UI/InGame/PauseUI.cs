582b29bd0218f83108c39763924ef41bd0c9a1bb
pid011 <pid011@kakao.com>
2021-12-08 01:28:22 +0900

    게임 나가기 UI 추가

---
+++ Assets/Scripts/UI/InGame/PauseUI.cs
@@ -0,0 +1,35 @@
+using Photon.Pun;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace EsperFightersCup
+{
+    public class PauseUI : MonoBehaviourPunCallbacks
+    {
+        [SerializeField]
+        private CanvasGroup _panel;
+
+        private void Awake()
+        {
+            _panel.gameObject.SetActive(false);
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                _panel.gameObject.SetActive(!_panel.gameObject.activeInHierarchy);
+            }
+        }
+
+        public void LeaveRoom()
+        {
+            PhotonNetwork.LeaveRoom();
+        }
+
+        public override void OnLeftRoom()
+        {
+            SceneManager.LoadScene("MainScene");
+        }
+    }
+}
