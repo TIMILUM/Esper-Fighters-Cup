f5bfbf5a1c3fa2c9abde325ca0c3b16bb2bcadd6
ounols <ounols@naver.com>
2021-12-07 22:01:48 +0900

    풍압 오브젝트에 적용되는 히트 시스템 일부 수정

--- Assets/Scripts/Object/Actor/StaticObject/WindLoadingObject.cs
+++ Assets/Scripts/Object/Actor/StaticObject/WindLoadingObject.cs
@@ -11,10 +11,20 @@ public class WindLoadingObject : AStaticObject
 
     private Coroutine _generateKnockBackCoroutine;
 
+    [SerializeField]
+    private ParticleSystem _particleSystem = null;
+
     protected override void Start()
     {
         base.Start();
         _colliderChecker.OnCollision += SetHit;
+        GetComponent<ObjectHitSystem>().IsDestroyable = false;
+    }
+
+    public override void SetHit(ObjectBase to)
+    {
+        _buffOnCollision[0].Damage = _appendBuffStack[0].Damage;
+        base.SetHit(to);
     }
 
     protected override void Update()
@@ -43,7 +53,7 @@ public class WindLoadingObject : AStaticObject
         }
 
         yield return new WaitUntil(() => BuffController.ActiveBuffs[BuffObject.Type.KnockBack].Count > 0);
-        yield return new WaitUntil(() => BuffController.ActiveBuffs[BuffObject.Type.KnockBack].Count <= 0);
+        yield return new WaitUntil(() => _particleSystem.isStopped);
 
         if (gameObject)
         {
